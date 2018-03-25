22be12bfbf22a988a461c76c98139f0be71e209f
tarikfojnica <tarik.f786@gmail.com>
2018-03-25 16:47:03 +0200

    Update the authenticate_card method

--- app.py
+++ app.py
@@ -2,22 +2,19 @@
 
 from flask import Flask, request
 
-app = Flask(__name__) #create the Flask app
+app = Flask(__name__)
 
 # Authenticate a credit card and get user access to his account in the ATM client.
 @app.route('/authenticate-card', methods=['POST'])
 def authenticate_card():
-    card_id = request.args.get('card_id')
+    req_data = request.get_json(force=True)
+    card_id = req_data['card_id']
 
-    return '''<h1>The card id is: {}</h1>'''.format(card_id)
+    return '''The card passed for reading is: {}'''.format(card_id)
 
 # Check the credit card's validity, balance ,and process the payment.
 @app.route('/process-payment', methods=['POST'])
 def process_payment():
-    return 'Todo...'
-
-@app.route('/json-example')
-def json_example():
     return 'Todo...'
 
 if __name__ == '__main__':
